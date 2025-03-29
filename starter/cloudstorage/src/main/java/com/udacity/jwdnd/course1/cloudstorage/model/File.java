22e58f0046942149cc86d68e175349be2971b5ad
ernest nnodu <ernest.nnodu@outlook.com>
2025-03-29 22:28:29 +0000

    Create file model class

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/File.java
@@ -0,0 +1,27 @@
+package com.udacity.jwdnd.course1.cloudstorage.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class File {
+
+    private Integer fileId;
+    private String filename;
+    private String contentType;
+    private String fileSize;
+    private Integer userId;
+    private byte[] fileData;
+
+    public File() {
+    }
+
+    public File(String filename, String contentType, String fileSize, Integer userId, byte[] fileData) {
+        this.filename = filename;
+        this.contentType = contentType;
+        this.fileSize = fileSize;
+        this.userId = userId;
+        this.fileData = fileData;
+    }
+}
