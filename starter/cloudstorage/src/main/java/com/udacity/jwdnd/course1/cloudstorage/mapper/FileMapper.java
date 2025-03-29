158e1ca7630774607fbd40f59af26e0c1f8992ad
ernest nnodu <ernest.nnodu@outlook.com>
2025-03-29 23:15:07 +0000

    Create file mapper to map file model to database

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -0,0 +1,24 @@
+package com.udacity.jwdnd.course1.cloudstorage.mapper;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.File;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface FileMapper {
+
+    @Select("SELECT * FROM FILES WHERE fileid = #{fileId}")
+    File getFileById(Integer fileId);
+
+    @Select("SELECT * FROM FILES WHERE userid = #{userId}")
+    List<File> getFilesByUserId(Integer userId);
+
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, filedata) " +
+            "VALUES (#{filename}, {contentType}, #{fileSize}, #{userId}, #{fileData})")
+    @Options(useGeneratedKeys = true, keyProperty = "fileid")
+    int insert(File file);
+
+    @Delete("DELETE FROM FILES WHERE fileid = #{fileId}")
+    int deleteFile(Integer fileId);
+}
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -14,7 +14,7 @@ public interface NoteMapper {
     @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
     Note getNoteById(Integer noteId);
 
-    @Select("SELECT FROM NOTES WHERE userid = #{userId}")
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
     List<Note> getNoteByUserId(Integer userId);
 
     @Select("INSERT INTO NOTES (notetitle, notedescription, userid) " +
