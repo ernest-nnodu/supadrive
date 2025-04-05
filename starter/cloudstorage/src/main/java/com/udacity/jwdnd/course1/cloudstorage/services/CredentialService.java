77b7697010449feba88e68e677f2dcfd797d3f21
ernest nnodu <ernest.nnodu@outlook.com>
2025-04-05 21:20:44 +0100

    Create credential service

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -0,0 +1,23 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CredentialService {
+
+    private final CredentialMapper credentialMapper;
+    private final EncryptionService encryptionService;
+
+    public CredentialService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
+        this.credentialMapper = credentialMapper;
+        this.encryptionService = encryptionService;
+    }
+
+    public int saveCredential(Credential credential) {
+
+        credential.setPassword(encryptionService.encryptValue(credential.getPassword(), credential.getUsername()));
+        return credentialMapper.insert(credential);
+    }
+}
